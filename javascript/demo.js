23e95f18c9bdc0236a9907ec3645910d1beb5ac4
jerome-larens <jeromelarens6@gmail.com>
2025-08-16 11:04:25 +0530

    code added

---
+++ javascript/demo.js
@@ -0,0 +1,77 @@
+// var a="apple";
+// var b=10;
+
+{
+    const a=10
+    a=20
+
+console.log(a)
+}
+// a="apple"
+// console.log(a)
+// var a=10
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+// var a=10
+// var b=a++
+// console.log(b)
+// console.log(a)
+// var kavi=200
+// var arr=[10,20,kavi,true];
+// console.log(arr)
+// var arr=[10,20,"kavi",true]
+// arr[0]=200
+// console.log(arr)
+// var arr=[10,20,"kavi",true]
+// arr.unshift(300)
+// console.log(arr)
+// var arr=[10,20,"kavi",true]
+// arr.shift()
+// console.log(arr)/
+
+// function
+
+// function add()
+// {
+//     var a=20
+//     var b=20
+//     console.log(a+b)
+// }
+// add()
+
+// function add(a,b)
+// {
+//     console.log(a+b)
+// }
+// add(20,30)
+
+// function add(a,b)
+// {
+//     return a+b
+// }
+//   a=add(20,60)
+ 
+// if
+
+// if(true)
+// {
+//     console.log("hello")
+// }
+// else{
+//     console.log("hi")
+// }/
+// var a=9
+// if(a==22)
+// {
+//     console.log(a)
+// }
+// else
+// {
+//     console.log("error")
+// }
+// for
+// for(i=0;i<6;++i)
+// {
+//     console.log(i)
+// }
